65dc5595db8b8fcc0f5024ca08086e42bd593feb
Elias Skogevall <elias.skogevall@gmail.com>
2021-02-08 15:01:39 +0100

    Initialize

---
+++ test/test.ts
@@ -0,0 +1,11 @@
+import * as tools from '@accility/protoc-tools'
+import * as gRPCWeb from '../dist/plugin'
+
+tools.protoc({
+	files: [ __dirname + '/main.proto' ],
+	includeDirs: [ __dirname ],
+	outDir:  __dirname + '/generated',
+	outOptions: [gRPCWeb({
+		module: 'commonjs'
+	})]
+})
