eda0d7da50fe1bd86839508ec6397e8e6584f552
Elias Skogevall <elias.skogevall@gmail.com>
2021-02-17 13:08:30 +0100

    Add JavaScript generator to test

--- test/test.ts
+++ test/test.ts
@@ -2,10 +2,15 @@ import * as tools from '@accility/protoc-tools'
 import * as gRPCWeb from '../dist/plugin'
 
 tools.protoc({
-	files: [ __dirname + '/main.proto' ],
-	includeDirs: [ __dirname ],
-	outDir:  __dirname + '/generated',
-	outOptions: [gRPCWeb({
-		module: 'commonjs'
-	})]
+	files: [__dirname + '/main.proto'],
+	includeDirs: [__dirname],
+	outDir: __dirname + '/generated',
+	outOptions: [
+		tools.generators.js({
+			outOptions: 'import_style=commonjs'
+		}),
+		gRPCWeb({
+			module: 'commonjs'
+		})
+	]
 })
